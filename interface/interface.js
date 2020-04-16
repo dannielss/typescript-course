f842ce630b3f8c639b9b1f3a758e2a77e8c7a607
Daniel <daniel_sousa.p@outlook.com>
2020-04-15 23:43:03 -0300

    interface

--- interface/interface.js
+++ interface/interface.js
@@ -7,9 +7,59 @@ function mudarNome(pessoa) {
 }
 var pessoa = {
     nome: 'João',
-    idade: 25
+    idade: 25,
+    saudar: function (sobrenome) {
+        console.log('Olá, meu nome é: ' + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
+// saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+pessoa.saudar('Skywalker');
+var Cliente = /** @class */ (function () {
+    function Cliente() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    Cliente.prototype.saudar = function (sobrenome) {
+        console.log('Olá, meu nome é: ', this.nome + ' ' + sobrenome);
+    };
+    return Cliente;
+}());
+var meuCliente = new Cliente;
+meuCliente.nome = 'Yuri';
+meuCliente.saudar('Boyka');
+console.log(meuCliente.ultimaCompra);
+var potencia;
+potencia = function (base, exp) {
+    return Math.pow(base, exp);
+};
+console.log(potencia(2, 2));
+var RealA = /** @class */ (function () {
+    function RealA() {
+    }
+    RealA.prototype.a = function () { };
+    return RealA;
+}());
+var RealAB = /** @class */ (function () {
+    function RealAB() {
+    }
+    RealAB.prototype.a = function () { };
+    RealAB.prototype.b = function () { };
+    return RealAB;
+}());
+var RealABC = /** @class */ (function () {
+    function RealABC() {
+    }
+    RealABC.prototype.a = function () { };
+    RealABC.prototype.b = function () { };
+    RealABC.prototype.c = function () { };
+    return RealABC;
+}());
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+var x = 2;
+x.log();
 //# sourceMappingURL=interface.js.map
--- interface/interface.ts
+++ interface/interface.ts
@@ -1,6 +1,8 @@
 interface Humano {
-  nome: string,
-  idade: number
+  nome: string
+  idade?: number
+  [prop: string]: any
+  saudar(sobrenome: string): void
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -11,11 +13,84 @@ function mudarNome(pessoa: Humano) {
   pessoa.nome = 'Joana'
 }
 
-const pessoa = {
+const pessoa: Humano = {
   nome: 'João',
-  idade: 25
+  idade: 25,
+  saudar(sobrenome: string) {
+    console.log('Olá, meu nome é: '+ this.nome + ' ' + sobrenome)
+  }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
+// saudarComOla({ nome: 'Jonas', idade: 27, altura: 1.75 })
+
+pessoa.saudar('Skywalker')
+
+class Cliente implements Humano {
+  nome: string = ''
+  ultimaCompra: Date = new Date
+
+  saudar(sobrenome: string) {
+    console.log('Olá, meu nome é: ', this.nome + ' ' + sobrenome)
+  }
+}
+
+const meuCliente = new Cliente
+
+meuCliente.nome = 'Yuri'
+meuCliente.saudar('Boyka')
+console.log(meuCliente.ultimaCompra)
+
+interface FuncaoCalculo {
+  (a: number, b:number ): number
+}
+
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number {
+  return base ** exp
+}
+
+console.log(potencia(2, 2))
+
+interface A {
+  a(): void
+}
+
+interface B {
+  b(): void
+}
+
+interface ABC extends A, B {
+  c(): void
+}
+
+class RealA implements A {
+  a(): void {}
+}
+
+class RealAB implements A, B {
+  a(): void {}
+  b(): void {}
+}
+
+class RealABC implements ABC {
+  a(): void {}
+  b(): void {}
+  c(): void {}
+}
+
+interface Object {
+  log(): void
+}
+
+Object.prototype.log = function() {
+  console.log(this.toString())
+}
+
+const x = 2;
+
+x.log()
