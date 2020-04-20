84ad5e2f8fb1178fe9174dd7ed192bf8e195b85b
Daniel <daniel_sousa.p@outlook.com>
2020-04-20 19:00:31 -0300

    decorators pt 2

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,10 +1,42 @@
-@logarClasse
+function logarClasse(construtor: Function) {
+  console.log(construtor)
+}
+
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+  return valor ? logarClasse : decoratorVazio
+}
+
+function decorator(a: string, b: number) {
+  return function(_: Function): void {
+    console.log(a + ' ' + b)
+  }
+}
+
+// @logarClasse
+// @logarClasseSe(true)
+// @decorator('Oi', 123)
+@logarObjeto
 class Eletrodomestico {
   constructor() {
     console.log('novo...')
   }
 }
 
-function logarClasse(construtor: Function) {
-  console.log(construtor)
+type Construtor = { new(...args: any[]): {} }
+
+function logarObjeto(construtor: Construtor) {
+  console.log('Carregado')
+  return class extends construtor {
+    constructor(...args: any[]) {
+      console.log('Antes...')
+      super(...args)
+      console.log('Depois...')
+    }
+  }
 }
+
+new Eletrodomestico()
+new Eletrodomestico()
+new Eletrodomestico()
